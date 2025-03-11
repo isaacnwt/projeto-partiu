3b793119811afd12ba452936935b3af523450b9d
isaacnwt <isaacnewton02@hotmail.com>
2025-03-11 20:35:25 -0300

    feat(front): adequando front a modelo novo da api

--- partiu-front/src/app/models/evento.model.ts
+++ partiu-front/src/app/models/evento.model.ts
@@ -1,8 +1,20 @@
+export interface Link {
+  label: string;
+  url: string;
+}
+
+export interface Contato {
+  nome: string;
+  telefone?: string;
+}
+
 export interface Evento {
   id: string;
   titulo: string;
-  descricao: string;
+  descricao?: string;
   data: string;
-  local: string;
+  endereco: string;
   imagem: string;
+  links?: Link[];
+  contatos?: Contato[];
 }
